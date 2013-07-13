afe17afdca040623f6d4a11491a0cfa89306eb9c
Himalaya Kakshapati <hkakshapati@gmail.com>
2013-07-13 11:20:19 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var fs=('fs');
 var app = express.createServer(express.logger());
-
+var buf=fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-    var buf=fs.readFileSync("index.html");  
+//    var buf=fs.readFileSync("index.html");  
     response.send(buf.toString());
 //  response.send('Hello World 2!');
 });
